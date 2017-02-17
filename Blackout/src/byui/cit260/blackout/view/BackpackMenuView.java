773130f1eff1dc515014d3d5cbccb906c7fd7d35
Mark Gallup <ke6tzc@gmail.com>
2017-02-16 20:03:34 -0800

    2/16/2017 Mark Gallup Game Menu View Layer

---
+++ Blackout/src/byui/cit260/blackout/view/BackpackMenuView.java
@@ -0,0 +1,12 @@
+
+package byui.cit260.blackout.view;
+
+/**
+ *
+ * @author mgallup
+ */
+public class BackpackMenuView {
+    
+    
+    
+}
--- Blackout/src/byui/cit260/blackout/view/GameMenuView.java
+++ Blackout/src/byui/cit260/blackout/view/GameMenuView.java
@@ -1,14 +1,124 @@
 
 package byui.cit260.blackout.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mgallup
  */
 public class GameMenuView {
 
-    void displayMenu() {
-        System.out.println("\n*** displayMenu stub function called ***");
+    private String promptMessage = "";
+    private String menu;
+    
+    public GameMenuView() {
+        this.menu = "\n"
+                + "\n----------------------------------------------------------"
+                + "\n | Game Menu"
+                + "\n----------------------------------------------------------"
+                + "\nM - Move to New Location ((Player can enter N,E,S,W"
+                + "\nV - View Map"
+                + "\nS - Status"
+                + "\nE - Examine"
+                + "\nT - Talk"
+                + "\nQ - Back to Main Menu"
+                + "\n-----------------------------------------------------------";
+        System.out.println(menu);
     }
+
+    public void displayGameMenuView() {
+        boolean done = false; // set flag to not done
+        do {
+                // prompt for and get players name
+                String menuOption = this.getMenuOption();
+                if (menuOption.toUpperCase().equals("Q"))  { //user wants to quit
+                    MainMenuView mainMenu = new MainMenuView();
+                    mainMenu.displayMainMenuView();
+                    return;
+                }
+                    
+                    // do the requested action and display the next view
+                   done = this.doAction(menuOption);
+                   
+                   
+        } while (!done);
+
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; // intialize to not valid
+        
+        while (!valid) { // loop while an invalid va;lue is entered
+            System.out.println("\n" + this.promptMessage);
+            
+            value = keyboard.nextLine(); // get next line typed on keyboard
+            value = value.trim(); // trim off leading and trailing blanks
+            
+            if (value.length() > 1) { // only one charachter
+                System.out.println("\nInvalid value: choose a correct value");
+                continue;
+            }
+            
+            break;  // end then loop
+        }
+        return value;
+    }
+
+    private boolean doAction(String choice) {
+
+            choice = choice.toUpperCase(); //convert choice to upper case
+            
+            switch (choice) {
+                case "M" : //create start new game
+                    this.moveLocation();
+                    break;
+                case "V" : // create save game
+                    this.viewMap();
+                    break;
+                case "S" : // Load game
+                    this.viewStatus();
+                    break;
+                case "E" : //view examine
+                    this.viewExamine();
+                    break;
+                case "T" : //view talk
+                    this.viewTalk();
+                    break;
+                //case "Q" : //back to Main Menu
+                  //  this.mainMenu();
+                    //break;
+                default :
+                    System.out.println("\n*** Invalid selection, try again");
+                    break;
+            }
+            return false;
+    }
+
+    private void moveLocation() {
+        System.out.println("\n*** moveLocation() function called ***");
+    }
+
+    private void viewMap() {
+        System.out.println("\n*** viewMap() function called ***");    }
+
+    private void viewStatus() {
+        System.out.println("\n*** viewStatus() function called ***");
+    }
+
+    private void viewExamine() {
+        System.out.println("\n*** viewExamine() function called ***");
+    }
+
+    private void viewTalk() {
+        System.out.println("\n*** viewTalk() function called ***");
+    }
+
+    //private void mainMenu() {
+      //  System.out.println("\n*** mainMenu() function called ***");
+  //  }
+
     
 }
