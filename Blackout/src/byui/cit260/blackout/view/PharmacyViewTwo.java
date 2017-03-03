7807d6ef4223484271b7ef53c624b1e34b97a977
Brian <Brian@192.168.88.106>
2017-03-02 18:36:07 -0700

    Location Control, Location View Menu, scene added menu options, pharmacy views 1, 2 3.   make it do stuff.

--- Blackout/src/byui/cit260/blackout/view/PharmacyViewTwo.java
+++ Blackout/src/byui/cit260/blackout/view/PharmacyViewTwo.java
@@ -10,5 +10,32 @@ package byui.cit260.blackout.view;
  * @author mgall
  */
 public class PharmacyViewTwo extends PharmacyScene {
+ 
+     public PharmacyViewTwo(){
+     
+
+        
+        super.setMapSymbol("\n"
+                + "\n        (_)"
+                + "\n _______//"
+                + "\n(________)"
+                + "\n \\      /"
+                + "\n  |    |"
+                + "\n /______\\"
+                + "\n(________)"
+                +"\n   _     _"  
+                +"\n  / \\   / \\" 
+                +"\n ( # ) ( 2 )"
+                +"\n  \\_/   \\_/");
+        
+        super.setDescription(" Pharmacy Room 2");
+        super.setNumItems(2);
+        super.setNonPlayers("Customer Bob");
+        
+       
+        
+        
+    }
+    
     
 }
