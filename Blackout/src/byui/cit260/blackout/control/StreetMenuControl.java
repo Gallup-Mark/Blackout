7306fa65cf4abab731b57e1c6ae358eea4eb2856
Brian <Brian@192.168.88.106>
2017-03-03 18:00:38 -0700

    added method to call the menu of the general street control

--- Blackout/src/byui/cit260/blackout/control/StreetMenuControl.java
+++ Blackout/src/byui/cit260/blackout/control/StreetMenuControl.java
@@ -1,6 +1,7 @@
 
 package byui.cit260.blackout.control;
 
+import byui.cit260.blackout.view.StreetMenuView;
 import byui.cit260.blackout.view.StreetViewOne;
 
 /**
@@ -10,11 +11,17 @@ import byui.cit260.blackout.view.StreetViewOne;
 public class StreetMenuControl {
     
     // class instances
+    public static StreetMenuView streetMenuView = new StreetMenuView();
     public static StreetViewOne streetViewOne = new StreetViewOne();
     
     
     public static void showstreetViewOne(){
-        streetViewOne.display();
+        
+    }
+    
+    
+    public static void showstreetMenu(){
+        streetMenuView.display();
     }
     
 }
