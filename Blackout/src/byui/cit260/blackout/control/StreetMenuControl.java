23a63428121b317b738e40b8e1a76ca0ef1bf0b9
Brian <Brian@192.168.88.106>
2017-03-03 18:54:07 -0700

    more menus updated

--- Blackout/src/byui/cit260/blackout/control/StreetMenuControl.java
+++ Blackout/src/byui/cit260/blackout/control/StreetMenuControl.java
@@ -11,12 +11,28 @@ import byui.cit260.blackout.view.StreetViewOne;
 public class StreetMenuControl {
     
     // class instances
-    public static StreetMenuView streetMenuView = new StreetMenuView();
-    public static StreetViewOne streetViewOne = new StreetViewOne();
+    //class menu for all subStreet Views
+    
+    private static String menu =  ("\n"
+                + "\n----------------------------------------------------------"
+                + "\n | Street Menu"
+                + "\n----------------------------------------------------------"
+                + "\n1 - Move to Street 1"
+                + "\n2 - Move to Street 2"
+                + "\n3 - Move to Street 3"
+                + "\n4 - Move to Street 4"
+                + "\n5 - Move to Street 5"
+                + "\nQ - Back to Main Menu"
+                + "\n-----------------------------------------------------------");
+    
+    private static StreetMenuView streetMenuView = new StreetMenuView();
+    private static StreetViewOne streetViewOne = new StreetViewOne(menu);
+    
     
     
     public static void showstreetViewOne(){
-        
+        streetViewOne.display();
+        System.out.println(menu);
     }
     
     
