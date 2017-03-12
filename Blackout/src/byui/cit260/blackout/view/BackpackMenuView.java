20fc37cfcc7054d0abc4a991d924b68b509ff10a
Brian <Brian@192.168.88.106>
2017-03-11 23:17:41 -0700

    took out extra space

--- Blackout/src/byui/cit260/blackout/view/BackpackMenuView.java
+++ Blackout/src/byui/cit260/blackout/view/BackpackMenuView.java
@@ -29,46 +29,7 @@ public class BackpackMenuView extends View {
                 + "\n-----------------------------------------------------------");
     }
         
-    /*public void displayBackpackMenuView() {
-        boolean done = false; // set flag to not done
-        do {
-                // prompt for and get players name
-                String menuOption = this.getMenuOption();
-                if (menuOption.toUpperCase().equals("Q")) { //user wants to quit
-                    
-                    return;
-                }
-                    
-                    // do the requested action and display the next view
-                   done = this.doAction(menuOption);
-                   
-                   
-        } while (!done);
-
-    }
     
-        private String getMenuOption() {
-                System.out.println(menu);
-
-        Scanner keyboard = new Scanner(System.in); // get infile for keyboard
-        String value = ""; //value to be returned
-        boolean valid = false; // intialize to not valid
-        
-        while (!valid) { // loop while an invalid va;lue is entered
-            System.out.println("\n" + this.promptMessage);
-            
-            value = keyboard.nextLine(); // get next line typed on keyboard
-            value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() > 1) { // only one charachter
-                System.out.println("\nInvalid value: choose a correct value");
-                continue;
-            }
-            
-            break;  // end then loop
-        }
-        return value;
-    }*/
         
     @Override    
     public boolean doAction(String choice) {
